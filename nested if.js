4cc4ebd75d8845b47b243495d130f624ec647c57
deepikagh <deepikaharikantra815@gmail.com>
2023-07-06 11:29:09 +0530

    js

---
+++ nested if.js
@@ -0,0 +1,17 @@
+var a = 25;
+if (a>75)
+{
+    console.log(Distinction);
+}
+else if(a>60)
+{
+    console.log("First class");
+}
+else if(a>40)
+{
+    console.log("Pass");
+}
+else
+{
+    console.log("Failed")
+}
